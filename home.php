401f69f60ec0967bb1a4d53a98bd31cac93b0ce0
monkey0318 <88133123+monkey0318@users.noreply.github.com>
2021-08-28 20:35:51 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>TARUC Basketball Society's Home Page</title>
+        <link href="home.css" rel="stylesheet">
+        <link rel="shortcut icon" type="image/x-icon" href="img/TARC.png">
+
+    </head>
+    <body>
+        <br>
+        <div class="container">
+            <div class="navbar">
+                <img src="img/logo.png" class="logo">
+                <nav>
+                    <ul>
+                        <li><a href="home.php">HOME</a></li>
+                        <li><a href="aboutus.php">ABOUT US</a></li>
+                        <li><a href="gallery.php">GALLERY</a></li>
+                        <li><a href="event.php">EVENT</a></li>
+                        <li><a href="shop.php">SHOP</a></li>
+                        <li><a href="contact.php">CONTACT US</a></li>
+                        <li><a href="profile.php">PROFILE</a></li>
+                        <li><a href="login.php">Login</a></li>
+                    </ul>
+                </nav>
+                <img src="img/menu.jpg" class="menu-icon">
+            </div>
+
+            <div class="col1">
+                <br><br><br>
+                <h2>Welcome to TARUC Basketball Society's Website.</h2>
+                <br>
+                <p>This is TARUC Basketball society, visit our website to look more details about us ...</p>
+                <button type="button">Search</button>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="card card1">
+                        <a href="event.php">
+                            <h5>Event</h5>
+                        </a>
+                        <p></p>
+
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card card2">
+                        <a href="aboutus.php">
+                            <h5>About Us</h5>
+                        </a>
+                        <p></p>
+
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card card3">
+                        <a href="shop.php">
+                            <h5>SHOP</h5>
+                        </a>
+                        <p></p>
+
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card card4">
+                        <a href="gallery.php">
+                            <h5>Gallery</h5>
+                        </a>
+                        <p></p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
